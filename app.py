cc209322abc1cd529c1856c45c126a8c0e0cffd2
Bhavesh979 <bhaveshchoudhary979@gmail.com>
2026-03-09 03:42:46 -0400

    Fix formatting and spacing in app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, request
 import analyzer
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
 
-    strength=None
-    feedback=[]
-    suggestion=None
-    entropy=None
+    strength = None
+    feedback = []
+    suggestion = None
+    entropy = None
 
-    if request.method=="POST":
+    if request.method == "POST":
 
-        password=request.form["password"]
+        password = request.form["password"]
 
-        strength,feedback,entropy=analyzer.check_password_strength(password)
+        strength, feedback, entropy = analyzer.check_password_strength(password)
 
-        suggestion=analyzer.generate_strong_password()
+        suggestion = analyzer.generate_strong_password()
 
     return render_template(
         "index.html",
@@ -26,6 +27,7 @@ def home():
         suggestion=suggestion,
         entropy=entropy
     )
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
