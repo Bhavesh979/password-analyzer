64b88d0fa8af5290169b33cf34dea87c32ec7dbf
Bhavesh979 <bhaveshchoudhary979@gmail.com>
2026-03-09 03:02:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,28 @@
 from flask import Flask, render_template, request
-import re
+import analyzer
 
 app = Flask(__name__)
 
-def check_password_strength(password):
-    score = 0
-    feedback = []
-
-    if len(password) >= 8:
-        score += 1
-    else:
-        feedback.append("Password should be at least 8 characters long.")
-
-    if re.search(r'[A-Z]', password):
-        score += 1
-    else:
-        feedback.append("Add at least one uppercase letter.")
-
-    if re.search(r'[a-z]', password):
-        score += 1
-    else:
-        feedback.append("Add at least one lowercase letter.")
-
-    if re.search(r'\d', password):
-        score += 1
-    else:
-        feedback.append("Add at least one digit.")
-
-    if re.search(r'[!@#$%^&*(),.?\":{}|<>]', password):
-        score += 1
-    else:
-        feedback.append("Add at least one special character.")
-
-    if score <= 2:
-        strength = "Weak"
-    elif score <= 4:
-        strength = "Moderate"
-    else:
-        strength = "Strong"
-
-    return strength, feedback
-
-
 @app.route("/", methods=["GET", "POST"])
-def index():
+def home():
+
     strength = None
     feedback = []
+    suggestion = None
 
     if request.method == "POST":
+
         password = request.form["password"]
-        strength, feedback = check_password_strength(password)
 
-    return render_template("index.html", strength=strength, feedback=feedback)
+        strength, feedback = analyzer.check_password_strength(password)
+        suggestion = analyzer.generate_strong_password()
 
+    return render_template(
+        "index.html",
+        strength=strength,
+        feedback=feedback,
+        suggestion=suggestion
+    )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
