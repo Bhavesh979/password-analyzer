b3f5d1346f3f7b16a427d2d02c1ffb76c4308718
Bhavesh979 <bhaveshchoudhary979@gmail.com>
2026-03-09 04:04:40 -0400

    Refactor password strength checker by removing entropy
    Removed unused calculate_entropy function and related entropy calculation from password strength check.

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,7 @@
 import re
 import random
 import string
-import math
+
 
 def check_password_strength(password):
 
@@ -13,22 +13,22 @@ def check_password_strength(password):
     else:
         feedback.append("Password should be at least 8 characters long.")
 
-    if re.search("[A-Z]", password):
+    if re.search(r"[A-Z]", password):
         score += 1
     else:
         feedback.append("Add at least one uppercase letter.")
 
-    if re.search("[a-z]", password):
+    if re.search(r"[a-z]", password):
         score += 1
     else:
         feedback.append("Add at least one lowercase letter.")
 
-    if re.search("\d", password):
+    if re.search(r"\d", password):
         score += 1
     else:
         feedback.append("Add at least one digit.")
 
-    if re.search("[!@#$%^&*(),.?\":{}|<>]", password):
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
         score += 1
     else:
         feedback.append("Add at least one special character.")
@@ -40,33 +40,7 @@ def check_password_strength(password):
     else:
         strength = "Strong"
 
-    entropy = calculate_entropy(password)
-
-    return strength, feedback, entropy
-
-
-def calculate_entropy(password):
-
-    charset = 0
-
-    if re.search("[a-z]", password):
-        charset += 26
-
-    if re.search("[A-Z]", password):
-        charset += 26
-
-    if re.search("\d", password):
-        charset += 10
-
-    if re.search("[!@#$%^&*(),.?\":{}|<>]", password):
-        charset += 32
-
-    if charset == 0:
-        return 0
-
-    entropy = len(password) * math.log2(charset)
-
-    return round(entropy,2)
+    return strength, feedback
 
 
 def generate_strong_password():
