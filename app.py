cad9976574e43b29eeb1b06901137c25e97ea6d4
Bhavesh979 <bhaveshchoudhary979@gmail.com>
2026-03-10 04:37:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,54 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import analyzer
+import firebase_admin
+from firebase_admin import credentials, firestore
 import os
+import time
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+# Firebase initialization
+cred = credentials.Certificate("firebase_key.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+@app.route("/")
 def home():
+    return render_template("index.html")
 
-    strength=None
-    feedback=[]
-    suggestion=None
-    score=0
-    password=""   # keep password value
 
-    if request.method=="POST":
+@app.route("/analyze", methods=["POST"])
+def analyze():
 
-        password=request.form["password"]
+    password = request.json["password"]
 
-        strength,feedback,score=analyzer.check_password_strength(password)
+    result = analyzer.check_password_strength(password)
 
-        # Only show suggestion if password is not strong
-        if strength != "Strong":
-            suggestion=analyzer.generate_strong_password()
+    entry_id = "PASS_" + str(int(time.time()*1000))
 
-    return render_template(
-        "index.html",
-        strength=strength,
-        feedback=feedback,
-        suggestion=suggestion,
-        score=score,
-        password=password
-    )
+    db.collection("password_analysis").add({
 
-if __name__=="__main__":
+        "EntryID": entry_id,
+        "Strength": result["strength"],
+        "Score": result["score"],
+        "Length": result["length"],
+        "Uppercase": result["has_upper"],
+        "Lowercase": result["has_lower"],
+        "Numbers": result["has_number"],
+        "SpecialChar": result["has_special"],
+        "CreatedAt": firestore.SERVER_TIMESTAMP
 
-    port=int(os.environ.get("PORT",10000))
+    })
 
-    app.run(host="0.0.0.0",port=port)
+    result["EntryID"] = entry_id
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(host="0.0.0.0", port=port)
