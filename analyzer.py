b7c915c8b4b1cb8f822c97aa8498d4d9b394cf91
Bhavesh979 <bhaveshchoudhary979@gmail.com>
2026-03-10 04:41:17 -0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -2,65 +2,50 @@ import re
 import random
 import string
 
-
 def check_password_strength(password):
 
-    score = 0
-    feedback = []
+    score=0
+    feedback=[]
 
-    has_upper = bool(re.search(r"[A-Z]", password))
-    has_lower = bool(re.search(r"[a-z]", password))
-    has_number = bool(re.search(r"[0-9]", password))
-    has_special = bool(re.search(r"[!@#$%^&*]", password))
-
-    if len(password) >= 8:
-        score += 1
+    if len(password)>=8:
+        score+=1
     else:
         feedback.append("Password must be at least 8 characters.")
 
-    if has_upper:
-        score += 1
+    if re.search(r"[A-Z]",password):
+        score+=1
     else:
-        feedback.append("Add uppercase letters.")
+        feedback.append("Add an uppercase letter.")
 
-    if has_lower:
-        score += 1
+    if re.search(r"[a-z]",password):
+        score+=1
     else:
-        feedback.append("Add lowercase letters.")
+        feedback.append("Add a lowercase letter.")
 
-    if has_number:
-        score += 1
+    if re.search(r"\d",password):
+        score+=1
     else:
-        feedback.append("Add numbers.")
+        feedback.append("Add a number.")
 
-    if has_special:
-        score += 1
+    if re.search(r"[!@#$%^&*]",password):
+        score+=1
     else:
-        feedback.append("Add special characters.")
+        feedback.append("Add a special character.")
 
-    if score <= 2:
-        strength = "Weak"
-    elif score <= 4:
-        strength = "Moderate"
+    if score<=2:
+        strength="Weak"
+    elif score<=4:
+        strength="Moderate"
     else:
-        strength = "Strong"
+        strength="Strong"
 
-    return {
-        "strength": strength,
-        "score": score,
-        "length": len(password),
-        "has_upper": has_upper,
-        "has_lower": has_lower,
-        "has_number": has_number,
-        "has_special": has_special,
-        "feedback": feedback
-    }
+    return strength,feedback,score
 
 
 def generate_strong_password():
 
-    chars = string.ascii_letters + string.digits + "!@#$%^&*"
+    chars=string.ascii_letters+string.digits+"!@#$%^&*"
 
-    password = ''.join(random.choice(chars) for _ in range(12))
+    password="".join(random.choice(chars) for _ in range(12))
 
     return password
